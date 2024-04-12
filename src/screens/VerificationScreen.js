5b13f91fd5a472f1547c012dfbc37f811d53b6f5
antonyt8 <antony8thiago@gmail.com>
2024-04-12 10:27:04 -0300

    deixando as telas certinhas

--- src/screens/VerificationScreen.js
+++ src/screens/VerificationScreen.js
@@ -1,19 +1,51 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, Modal } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, Modal, ActivityIndicator } from 'react-native';
 
 const VerificationScreen = () => {
   const [code, setCode] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [senha, setSenha] = useState('');
   const [modalVisible, setModalVisible] = useState(false); // Estado para controlar a visibilidade do modal
+  const [progressLoad, setProgressLoad] = useState(false); // Estado para controlar o indicador de progresso
+
+  const handleLogin = () => {
+    // Lógica de login aqui
+    // Abre o modal ao clicar em "Entrar"
+    setModalVisible(true);
+  };
 
   const verifyCode = () => {
     // Lógica para verificar o código de verificação
     if (code === '123456') {
       // Código de verificação correto, exibe o modal
-      setModalVisible(true);
+      // setModalVisible(true);
+      console.log('Código de verificação correto');
+      alert('Código de verificação correto');
     } else {
       // Código de verificação incorreto, você pode exibir uma mensagem de erro para o usuário
+      console.log('Código de verificação incorreto');
       alert('Código de verificação incorreto');
     }
+  };
+
+  const verifyLogin = () => {
+    // Lógica para verificar o código de verificação
+    if (cpf === '12345678' && senha === '12345678' ) {
+      // Código de verificação correto, exibe o modal
+      setModalVisible(true);
+      console.log('Código de verificação correto');
+      alert('Código de verificação correto');
+    } else {
+      // Código de verificação incorreto, você pode exibir uma mensagem de erro para o usuário
+      console.log('Código de verificação incorreto');
+      alert('Código de verificação incorreto');
+    }
+  };
+
+  const handleForgotPassword = () => {
+    // Lógica para ir para a tela de redefinição de senha
+    // Você precisa implementar a navegação para a tela 'EsqueceuSenha' aqui
+    console.log('Ir para a tela de redefinição de senha');
   };
 
   return (
@@ -23,19 +55,37 @@ const VerificationScreen = () => {
         style={styles.logo}
         resizeMode="contain"
       />
-      <Text style={styles.title}>Insira o código de verificação</Text>
+      <Text style={styles.title}>LOGIN</Text>
       
       <TextInput
         style={styles.input}
-        value={code}
-        onChangeText={setCode}
-        placeholder="Código de verificação"
+        value={cpf}
+        onChangeText={setCpf}
+        placeholder="digite seu cpf"
         keyboardType="numeric"
-        maxLength={6}
+        maxLength={11}
       />
-      <TouchableOpacity style={styles.button} onPress={verifyCode}>
-        <Text style={styles.buttonText}>Verificar código</Text>
-      </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        value={senha}
+        onChangeText={setSenha}
+        placeholder="Digite sua senha"
+        secureTextEntry={true}
+        maxLength={11} // Esta propriedade oculta o texto digitado
+      />
+
+      <View>
+        {progressLoad ? (
+          <ActivityIndicator color="#c7222c" size="large" />
+        ) : (
+          <TouchableOpacity style={styles.button} onPress={verifyLogin}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity onPress={handleForgotPassword}>
+          <Text style={styles.forgotPasswordText}>Esqueci minha senha?</Text>
+        </TouchableOpacity>
+      </View>
 
       {/* Modal */}
       <Modal
@@ -46,9 +96,25 @@ const VerificationScreen = () => {
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <Text style={styles.modalText}> Código de verificação correto! </Text>
+            <Image
+              source={require('./image.png')}
+              style={styles.logo}
+              resizeMode="contain"
+            />
+            <Text style={styles.title}>Insira o código de verificação</Text>
+            <TextInput
+              style={styles.input}
+              value={code}
+              onChangeText={setCode}
+              placeholder="Código de verificação"
+              keyboardType="numeric"
+              maxLength={6}
+            />
+            <TouchableOpacity style={styles.button} onPress={verifyCode}>
+              <Text style={styles.buttonText}>Verificar código</Text>
+            </TouchableOpacity>
             <TouchableOpacity style={styles.modalButton} onPress={() => setModalVisible(false)}>
-              <Text style={styles.modalButtonText}> Continuar </Text>
+              <Text style={styles.modalButtonText}>Fechar</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -107,15 +173,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: 'center',
   },
-  modalText: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
   modalButton: {
     backgroundColor: 'red',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 20,
+    marginTop: 20, // Adicionando margem superior para separar os botões
   },
   modalButtonText: {
     color: 'white',
